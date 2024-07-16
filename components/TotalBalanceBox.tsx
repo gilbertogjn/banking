17c9e97a38df7ad23691ee7ca05aff8ad13f9f29
Gilberto Garcia <gilbertogjn@gmail.com>
2024-07-16 16:37:48 -0400

    make total balance props

---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TotalBalanceBox = ({
+  accounts = [],
+  totalBanks,
+  totalCurrentBalance,
+}: TotalBalanceBoxProps) => {
+  return <div>TotalBalanceBox</div>;
+};
+
+export default TotalBalanceBox;

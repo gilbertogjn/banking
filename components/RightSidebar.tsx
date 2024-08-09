ec37e2be7fbc15b24782ad9deabc090adcb6ea20
Gilberto Garcia <gilbertogjn@gmail.com>
2024-08-09 17:07:51 -0400

    add right sidebar

---
+++ components/RightSidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RightSidebar = ({ user, transactions, banks}: RightSidebarProps) => {
+  return (
+    <aside className='right-sidebar'>
+        <section className='flex flex-col pb-8'>
+        </section>
+    </aside>
+  )
+}
+
+export default RightSidebar
